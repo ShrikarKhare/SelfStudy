984a06ef73cd2efc5e07c44311bf6248c0a475de
Shri Khare <khare@exploremetakinetic.com>
2022-03-15 17:35:19 -0400

    added tree includes

---
+++ Structy_Exercises/Binary_Trees/tree_includes.py
@@ -0,0 +1,90 @@
+'''
+Write a function, tree_includes, that takes in the root of a binary tree and a target value. 
+The function should return a boolean indicating whether or not the value is contained in the tree.
+
+test_00:
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+tree_includes(a, "e") # -> True
+test_01:
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+tree_includes(a, "a") # -> True
+'''
+class Node:
+    def __init__(self,val):
+        self.val = val
+        self.left = None 
+        self.right = None 
+
+from collections import deque
+def tree_includes(root,target):
+    queue = deque([root])
+
+    while queue:
+        current = queue.popleft()
+        if current.val == target:
+            return True
+        if current.left:
+            queue.append(current.left)
+        if current.right:
+            queue.append(current.right)
+    return False
+
+def tree_includes(root,target):
+    if not root: return False
+    if root.val == target: return True 
+
+    return tree_includes(root.left, target) or tree_includes(root.right, target)
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+print(tree_includes(a, 'd'))
+
+'''
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+'''
---
+++ Structy_Exercises/Binary_Trees/tree_sum.py
@@ -0,0 +1,100 @@
+'''
+Write a function, tree_sum, that takes in the root of a binary tree that contains number values. 
+The function should return the total sum of all values in the tree.
+
+test_00:
+a = Node(3)
+b = Node(11)
+c = Node(4)
+d = Node(4)
+e = Node(-2)
+f = Node(1)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#       3
+#    /    \
+#   11     4
+#  / \      \
+# 4   -2     1
+
+tree_sum(a) # -> 21
+test_01:
+a = Node(1)
+b = Node(6)
+c = Node(0)
+d = Node(3)
+e = Node(-6)
+f = Node(2)
+g = Node(2)
+h = Node(2)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+e.left = g
+f.right = h
+
+#      1
+#    /   \
+#   6     0
+#  / \     \
+# 3   -6    2
+#    /       \
+#   2         2
+
+tree_sum(a) # -> 10
+'''
+class Node:
+    def __init__(self,val):
+        self.val = val 
+        self.left = None
+        self.right = None
+
+# Breadth first (Iterative)
+from collections import deque
+def tree_sum(root):
+    if not root: return 0
+    queue = deque([root])
+    total = 0
+    while queue:
+        current = queue.popleft()
+        total += current.val
+        if current.left:
+            queue.append(current.left)
+        if current.right:
+            queue.append(current.right)
+    
+    return total
+
+#depth first (recursive)
+
+def tree_sum(root):
+    if not root: return 0
+    return root.val + tree_sum(root.left) + tree_sum(root.right)
+
+a = Node(1)
+b = Node(6)
+c = Node(0)
+d = Node(3)
+e = Node(-6)
+f = Node(2)
+g = Node(2)
+h = Node(2)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+e.left = g
+f.right = h
+
+
+print(tree_sum(a))
